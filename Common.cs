9dcde5a45296bbb99acc8ac25265683cb4bd653e
fhnaseer <fhnaseer@live.com>
2018-07-03 00:14:57 +0200

    Adding first function, using pre release version of microsoft.azure.servicebus,

---
+++ Common.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace AzurePubSubServerlessCSharp
+{
+    public static class Common
+    {
+        public const string StorageConnectionString = "StorageConnectionString";
+        public const string ServiceBusConnectionString = "ServiceBusConnectionString";
+
+        public static string GetEnvironmentVariable(string name)
+        {
+            return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
+
+        public static string GetServiceBusConnectionString()
+        {
+            return Common.GetEnvironmentVariable(Common.ServiceBusConnectionString);
+        }
+
+        public static string GetStorageConnectionString()
+        {
+            return Common.GetEnvironmentVariable(Common.ServiceBusConnectionString);
+        }
+
+        private static ManagementClient GetServiceBusClient()
+        {
+            return new ManagementClient(Common.GetEnvironmentVariable(Common.ServiceBusConnectionString));
+        }
+
+        public static Subscriber CreateSubscriberQueue(string id)
+        {
+            var client = GetServiceBusClient();
+            client.CreateQueueAsync(id, CancellationToken.None);
+            return new Subscriber
+            {
+                ConnectionString = Common.GetServiceBusConnectionString(),
+                QueueName = id
+            };
+        }
+    }
+}
--- Subscribe.cs
+++ Subscribe.cs
@@ -1,30 +1,40 @@
-
-using System.IO;
-using Microsoft.AspNetCore.Mvc;
+using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 
-namespace fhnaseer.PubSubServerless
+namespace AzurePubSubServerlessCSharp
 {
+    public class Subscriber
+    {
+        [JsonProperty("connectionString")]
+        public string ConnectionString { get; set; }
+
+        [JsonProperty("queueName")]
+        public string QueueName { get; set; }
+
+        [JsonProperty("QueueUrl")]
+        public string QueueUrl { get; set; }
+    }
+
     public static class Subscribe
     {
         [FunctionName("Subscribe")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static Subscriber Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
         {
-            log.Info("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            var guid = Guid.NewGuid().ToString().Replace("-", "");
+            var id = $"subscriber{guid}";
+            try
+            {
+                return Common.CreateSubscriberQueue(id);
+            }
+            catch (Exception e)
+            {
+                log.Info(e.Message);
+                return null;
+            }
         }
     }
 }
