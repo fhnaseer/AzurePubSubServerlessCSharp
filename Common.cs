30dd8fee18cc51c9e76526338c3ad12c75df21d3
fhnaseer <fhnaseer@live.com>
2018-07-07 00:51:26 +0200

    Adding subcribercontent function,

--- Common.cs
+++ Common.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Threading;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.ServiceBus.Management;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
 
 namespace AzurePubSubServerlessCSharp
 {
@@ -9,6 +14,10 @@ namespace AzurePubSubServerlessCSharp
         public const string StorageConnectionString = "StorageConnectionString";
         public const string ServiceBusConnectionString = "ServiceBusConnectionString";
 
+        public const string ContentsTableName = "content";
+        public const string TopicsTableName = "topics";
+        public const string FunctionsTableName = "functions";
+
         public static string GetEnvironmentVariable(string name)
         {
             return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
@@ -16,17 +25,23 @@ namespace AzurePubSubServerlessCSharp
 
         public static string GetServiceBusConnectionString()
         {
-            return Common.GetEnvironmentVariable(Common.ServiceBusConnectionString);
+            return GetEnvironmentVariable(ServiceBusConnectionString);
         }
 
         public static string GetStorageConnectionString()
         {
-            return Common.GetEnvironmentVariable(Common.ServiceBusConnectionString);
+            return GetEnvironmentVariable(StorageConnectionString);
         }
 
         private static ManagementClient GetServiceBusClient()
         {
-            return new ManagementClient(Common.GetEnvironmentVariable(Common.ServiceBusConnectionString));
+            return new ManagementClient(GetEnvironmentVariable(ServiceBusConnectionString));
+        }
+
+        public static T GetPostObject<T>(HttpRequest request)
+        {
+            var requestBody = new StreamReader(request.Body).ReadToEnd();
+            return JsonConvert.DeserializeObject<T>(requestBody);
         }
 
         public static Subscriber CreateSubscriberQueue(string id)
@@ -35,9 +50,26 @@ namespace AzurePubSubServerlessCSharp
             client.CreateQueueAsync(id, CancellationToken.None);
             return new Subscriber
             {
-                ConnectionString = Common.GetServiceBusConnectionString(),
+                ConnectionString = GetServiceBusConnectionString(),
                 QueueName = id
             };
+        }
+
+        public static Subscriber GetSubscriberResponse(string id)
+        {
+            return new Subscriber
+            {
+                ConnectionString = GetServiceBusConnectionString(),
+                QueueName = id
+            };
+        }
+
+        public static CloudTable GetAzureTable(string tableName)
+        {
+            var connectionString = GetStorageConnectionString();
+            var storageAccount = CloudStorageAccount.Parse(connectionString);
+            var tableClient = storageAccount.CreateCloudTableClient();
+            return tableClient.GetTableReference(tableName);
         }
     }
 }
